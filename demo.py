05a16325b2828d22df665c62bbbc8ef883e67318
Antoine Beyeler <abeyeler@ab-ware.com>
2025-10-26 21:02:41 +0100

    improve stuff + comments

--- demo.py
+++ demo.py
@@ -1,3 +1,13 @@
+# NOTE: this is a single-file Python module. More complex project should be organized into a package structure, which
+# would look like this:
+#
+# demo/
+#   __init__.py (often empty for application)
+#   __main__.py (this is executed when running `python -m demo`
+#   demo.py (actual code)
+#   ... (more actual code in other .py files)
+
+
 def add(a, b):
     return a + b
 
--- test_demo.py
+++ test_demo.py
@@ -1,3 +1,8 @@
+# Basic test file. Is automatically found by pytest thanks to its name starting with "test_". All functions starting
+# with "test_" are automatically discovered and run by pytest.
+#
+# More complex project would have many `test_XXX.py` files in a `tests/` directory.
+
 from demo import add
 
 
